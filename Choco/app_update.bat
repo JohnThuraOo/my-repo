49fda8f1b46d1e3a016f7188b557e0ec671fb650
Thura Oo <143941813+JohnThuraOo@users.noreply.github.com>
2025-12-16 12:03:18 +0630

    upload
    script

---
+++ Choco/app_update.bat
@@ -0,0 +1,10 @@
+@echo off
+start "" choco install googlechrome -y --force --ignore-checksums
+start "" choco install Firefox -y --force --ignore-checksums
+start "" choco install teamviewer -y --force --ignore-checksums
+start "" choco install wechat -y --force --ignore-checksums
+start "" choco install line -y --force --ignore-checksums
+start "" choco install zoom -y --force --ignore-checksums
+start "" choco install telegram -y --force --ignore-checksums
+start "" choco install viber -y --force --ignore-checksums
+start "" choco install vlc -y --force --ignore-checksums
